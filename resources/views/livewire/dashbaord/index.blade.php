a2afe20e1c50b038d6bdd7f17c18747a22602621
Ali Ghasemzadeh <alighasemzadeh.dev@gmail.com>
2021-08-20 19:25:40 +0430

    Show Duration In Index.

--- resources/views/livewire/dashbaord/index.blade.php
+++ resources/views/livewire/dashbaord/index.blade.php
@@ -13,6 +13,7 @@
                         <button class="rounded-full focus:outline-none focus:ring-2 focus:ring-indigo-600" wire:click='$emit("openModal", "task.edit", {{ json_encode(["task" => $task->id]) }})'><x-icon name="pencil-alt" class="w-4 h-4 text-gray-500" /></button>
 
                     </x-slot>
+                    {{ __('dolist.duration') }}:{{ $task->duration }}
                     {{ $task->description }}
                 </x-card>
             </div>
